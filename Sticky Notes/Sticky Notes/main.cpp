512968fe59a017a6b29472842c0d93b80101f5e2
Deema Hafiz <hafezdema1999@gmail.com>
2019-07-18 23:53:34 +0300

    updates on menu code

--- Sticky Notes/Sticky Notes/main.cpp
+++ Sticky Notes/Sticky Notes/main.cpp
@@ -1,4 +1,8 @@
-//Sticky Notes Program
+/*Sticky Notes Program
+
+This program allow users to write thier notes ,save them and retrieve them whenever they want.
+
+*/
 #include<iostream>
 using namespace std;
 int main()
@@ -6,6 +10,9 @@ int main()
 	int choice;
 	while(1)
 	{
+		/*creat the menu so from it the user would like to be asked to enter his/her user name, then enter a text message. 
+          Later on, the user should be able to retrieve all his/her messages.*/
+		
 		cout<<"Welcome to the brand new 'Sticky Notes'\nHere is the list of operation this program offers:\n";
 		cout<<"1- Add new user\n";
 		cout<<"2- Add new note\n";
