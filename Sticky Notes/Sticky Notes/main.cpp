610f93e1dc5b6925d46c1ec35a19910656c6b184
Deema Hafiz <hafezdema1999@gmail.com>
2019-07-18 22:02:52 +0300

    Add menu

--- Sticky Notes/Sticky Notes/main.cpp
+++ Sticky Notes/Sticky Notes/main.cpp
@@ -3,13 +3,20 @@
 using namespace std;
 int main()
 {
-	
+	int choice;
+	while(1)
+	{
+		cout<<"Welcome to the brand new 'Sticky Notes'\nHere is the list of operation this program offers:\n";
+		cout<<"1- Add new user\n";
+		cout<<"2- Add new note\n";
+		cout<<"3- View notes for a specific user\n";
+		cout<<"4- Exit\n";
+		cout<<"Enter your choice : ";
+		cin >> choice;
 
-
-
-
-
-
+		system("cls");
+	}
+	       
 
 
 	system("pause");
