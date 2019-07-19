878ef023d45a42e2d6187c413badad41789d6fa0
latifa.masri1998@gmail.com <latifa.masri1998@gmail.com>
2019-07-19 10:34:36 +0300

    add new user on user branch

--- Sticky Notes/Sticky Notes/main.cpp
+++ Sticky Notes/Sticky Notes/main.cpp
@@ -1,6 +1,26 @@
 //Sticky Notes Program
 #include<iostream>
+#include<fstream>//library for files
+#include<string>
 using namespace std;
+
+void addNewUser()
+{
+	string firstName,lastName,fullName;
+	cout<<"Welcome aboard new user!\nPlease let me know your first name: ";
+	cin>>firstName;
+    cout<<"Great "<<firstName<<" now please enter your last name: "; 
+	cin>>lastName;
+	cout<<"Done!\nNice to meet you "<<firstName<<" "<<lastName<<endl<<endl;
+
+	//open a file for a new user to write on it 
+	ofstream outfile;
+	fullName=firstName+"_"+lastName+".txt";
+	//creat file with user name
+	outfile.open(fullName.c_str());
+	outfile.close();
+
+}
 int main()
 {
 	int choice;
@@ -15,6 +35,14 @@ int main()
 		cin >> choice;
 
 		system("cls");
+		switch(choice)
+		{
+		case 1:
+			{
+				addNewUser();
+			}
+		}
+
 	}
 	       
 
