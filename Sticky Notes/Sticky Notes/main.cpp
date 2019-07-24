25134b2d07bfe1ae53cddd0aba9ca1a11904acc4
latifa.masri1998@gmail.com <latifa.masri1998@gmail.com>
2019-07-24 11:07:01 +0300

    final version

--- Sticky Notes/Sticky Notes/main.cpp
+++ Sticky Notes/Sticky Notes/main.cpp
@@ -20,9 +20,9 @@ void addNewUser()
 
 	//open a file for a new user to write on it 
 	ofstream outfile;
-	fullName=firstName+"_"+lastName+".txt";
+	fullName = firstName+"_"+lastName+".txt";
 	//creat file with user name
-	outfile.open(fullName.c_str());
+	outfile.open(fullName);
 	outfile.close();
 
 }
@@ -49,7 +49,7 @@ void addNote()
 		int hour = ltm->tm_hour; //(ltm->tm_hour) return hours since midnight.
 		int min =  ltm->tm_min; //(ltm->tm_min) return minutes after the hour.
 		ofstream outfile;
-		outfile.open(fullName.c_str(),ios::app);//open the file with intend to append notes to it.
+		outfile.open(fullName,ios::app);//open the file with intend to append notes to it.
 		outfile << month<<"/"<<day<<"/"<<year<<" "<<hour<<":"<<min<<endl;
 		outfile << note <<"\n\n";
 		cout<<"\nYour note has been well received, 1 second while saving it ...\nDone!\n\n";
@@ -68,15 +68,15 @@ void viewNotes()
 	cout<<"Retrieve your notes? Absolutely!\nPlease let know your full name first: ";
 	cin>>firstName>>lastName;
 	ifstream infile;
-    fullName=firstName+"_"+lastName+".txt";
-    infile.open(fullName.c_str());  
+    fullName = firstName+"_"+lastName+".txt";
+    infile.open(fullName);  
 	//this condition check if the user's file not exist then ask the user to check his/her name and if this is first time ask him/her to create a new user and then enter own notes 
     if(!infile) 
 	{
 		cout<<"Oh! Sorry the user name was not found, please check the name again and if this is your first time here, please go ahead and create a new user from the main menu and add notes"<<endl;
 	    return ;
 	}
-	//this condition check if the file is empty or not st if empty the start(peek) of the file equal to the end of the file
+	//this condition check if the file is empty or not exist if empty the start(peek) of the file equal to the end of the file
 	if ( infile.peek() == EOF )
       {
 		  cout<<"\nUmm, can’t find any saved notes for you."<<endl<<endl;
@@ -91,9 +91,8 @@ void viewNotes()
 		  while(getline(infile,str))//using to read the file line by line
 		  {
 			   
-			   printf("%s",str.c_str());//using for printing as cout
-			   cout<<endl;
-			   
+			   cout<<str<<endl;//using for printing as cout
+			  
 		  }
 
 		  
@@ -110,7 +109,7 @@ int main()
 	{
 		/*creat the menu so from it the user would like to be asked to enter his/her user name, then enter a text message. 
           Later on, the user should be able to retrieve all his/her messages.*/
-		
+		system("cls");
 		cout<<"Welcome to the brand new 'Sticky Notes'\nHere is the list of operation this program offers:\n";
 		cout<<"1- Add new user\n";
 		cout<<"2- Add new note\n";
@@ -118,23 +117,29 @@ int main()
 		cout<<"4- Exit\n";
 		cout<<"Enter your choice : ";
 		cin >> choice;
-
 		system("cls");
+
 		switch(choice)
 		{
 		case 1:
 			{
 				addNewUser();
+				cout<<"Press 'Enter' to return to main menu";
+				system("pause>nul");
 				break;
 			}
 		 case 2:
 			{
 				addNote();
+				cout<<"Press 'Enter' to return to main menu";
+				system("pause>nul");
 				break;
 			}
 		 case 3:
 			{
 				viewNotes();
+				cout<<"Press 'Enter' to return to main menu";
+				system("pause>nul");
 				break;
 			}
 		 case 4:
@@ -142,8 +147,7 @@ int main()
 				cout<<"Thank you for using our Sticky Notes program";
 				return 0;
 			}
-		 
-
+		
 		}
 
 	}
